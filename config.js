91f7c9bf061f6fe50707a0ac9a5f95af018739de
karan katiyar <karankatiyar@karans-MacBook-Pro.local>
2020-05-06 13:25:06 -0500

    codes merged

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/whiteboard'
+    'mongoUrl' : 'mongodb+srv://admin:KtTTfwsYnUbD7uK@cluster0-qqyyu.mongodb.net/whiteBoard?retryWrites=true&w=majority'
 }
